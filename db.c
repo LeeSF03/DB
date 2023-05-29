e5451338647490ae990e2a08a19dde2e7c85d918
LeeSF03 <shuenfeilee@gmail.com>
2023-05-29 09:54:39 +0800

    use strtok to toknized the input statement instead of sscanf

--- db.c
+++ db.c
@@ -27,7 +27,9 @@ typedef enum
 {
   PREPARE_SUCCESS,
   PREPARE_UNRECOGNIZED_STATEMENT,
-  PREPARE_SYNTAX_ERROR
+  PREPARE_SYNTAX_ERROR,
+  PREPARE_NEGATIVE_ID,
+  PREPARE_STRING_TOO_LONG,
 } PrepareResult;
 
 typedef enum
@@ -48,8 +50,8 @@ typedef enum
 typedef struct
 {
   uint32_t id;
-  char username[COLUMN_USERNAME_SIZE];
-  char email[COLUMN_EMAIL_SIZE];
+  char username[COLUMN_USERNAME_SIZE + 1];
+  char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 typedef struct
@@ -86,6 +88,7 @@ void read_input(InputBuffer *input_buffer);
 void close_input(InputBuffer *input_buffer);
 MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table);
 PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement);
+PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement);
 ExecuteResult execute_statement(Statement *statement, Table *table);
 Table *new_table();
 void free_table(Table *table);
@@ -125,7 +128,13 @@ int main(int argc, char *argv[])
     case (PREPARE_SUCCESS):
       break;
     case (PREPARE_SYNTAX_ERROR):
-        printf("Syntax erroe. Could not parse statement");
+      printf("Syntax error. Could not parse statement\n");
+      continue;
+    case (PREPARE_NEGATIVE_ID):
+      printf("Id number can't be negative\n");
+      continue;
+    case (PREPARE_STRING_TOO_LONG):
+      printf("String is too long\n");
       continue;
     case (PREPARE_UNRECOGNIZED_STATEMENT):
       printf("Unrecognized keyword at the start of '%s'\n", input_buffer->buffer);
@@ -229,13 +238,7 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
 {
   if (strncmp(input_buffer->buffer, "insert", 6) == 0)
   {
-    statement->type = STATEMENT_INSERT;
-    int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), &(statement->row_to_insert.username), &(statement->row_to_insert.email));
-    if (args_assigned < 3)
-    {
-      return PREPARE_SYNTAX_ERROR;
-    }
-    return PREPARE_SUCCESS;
+    return prepare_insert(input_buffer, statement);
   }
   /* not sure why this uses strcmp instead of strncmp */
   if (strcmp(input_buffer->buffer, "select") == 0)
@@ -245,6 +248,37 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
   }
 
   return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement)
+{
+  statement->type = STATEMENT_INSERT;
+  char *keyword = strtok(input_buffer->buffer, " ");
+  char *id_string = strtok(NULL, " ");
+  char *username = strtok(NULL, " ");
+  char *email = strtok(NULL, " ");
+
+  if (id_string == NULL || username == NULL || email == NULL)
+  {
+    return PREPARE_SYNTAX_ERROR;
+  }
+
+  int id = atoi(id_string);
+  if (id < 0)
+  {
+    return PREPARE_NEGATIVE_ID;
+  }
+
+  if (strlen(username) > COLUMN_USERNAME_SIZE || strlen(email) > COLUMN_EMAIL_SIZE)
+  {
+    return PREPARE_STRING_TOO_LONG;
+  }
+
+  statement->row_to_insert.id = id;
+  strcpy(statement->row_to_insert.username, username);
+  strcpy(statement->row_to_insert.email, email);
+
+  return PREPARE_SUCCESS;
 }
 
 /* function for executing statement */
@@ -323,7 +357,9 @@ ssize_t getline(char **line_ptr, size_t *buffer_len_ptr, FILE *stream)
     return -1;
   }
 
+  /* get the first character of input */
   c = getc(stream);
+
   /* skip over all the initial whitespace */
   /* while ((c = getc(stream)) == ' '); */
 
